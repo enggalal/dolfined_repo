339d88985b72f3701de57773a670f85aef26a2e4
enggalal <73644718+enggalal@users.noreply.github.com>
2023-01-08 18:57:17 +0400

    Update example.tf
    adding three users in the Dev branch.

--- example.tf
+++ example.tf
@@ -1,13 +1,13 @@
-# resource "aws_iam_user" "lb" {
-#   name  = "dev_user${count.index}"
-#   count = 3
-#   path  = "/system/"
-# }
+resource "aws_iam_user" "lb" {
+  name  = "dev_user${count.index}"
+  count = 3
+  path  = "/system/"
+}
 
-# output "iam_names" {
-#   value = aws_iam_user.lb[*].name
-# }
+output "iam_names" {
+  value = aws_iam_user.lb[*].name
+}
 
-# output "iam_arn" {
-#   value = aws_iam_user.lb[*].arn
-# }
+output "iam_arn" {
+  value = aws_iam_user.lb[*].arn
+}
